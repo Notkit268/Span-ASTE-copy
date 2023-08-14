ba9cb075941e438036d4c4f64b460c6dad755fbb
Notkit268 <67222376+Notkit268@users.noreply.github.com>
2023-08-15 00:54:35 +0300

    Update span_model.py

--- span_model/predictors/span_model.py
+++ span_model/predictors/span_model.py
@@ -2,7 +2,7 @@ from typing import List
 import numpy as np
 import warnings
 
-from overrides import overrides
+from overrides import override
 import numpy
 import json
 
@@ -34,13 +34,13 @@ class SpanModelPredictor(Predictor):
         instance = self._words_list_to_instance(tokenized_document)
         return self.predict_instance(instance)
 
-    @overrides
+    @override(check_signature=False)
     def dump_line(self, outputs):
         # Need to override to tell Python how to deal with Numpy ints.
         return json.dumps(outputs, default=int) + "\n"
 
     # TODO: Can this be implemented in `forward_on_instance`  instead?
-    @overrides
+    @override(check_signature=False)
     def predict_instance(self, instance):
         """
         An instance is an entire document, represented as a list of sentences.
