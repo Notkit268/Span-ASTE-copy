ed3c2494ae8837e113b623eff8a9a5847397781d
Notkit268 <67222376+Notkit268@users.noreply.github.com>
2023-08-16 13:14:43 +0300

    Update relation_proper.py

--- span_model/models/relation_proper.py
+++ span_model/models/relation_proper.py
@@ -3,7 +3,7 @@ from typing import Any, Dict, List, Optional, Callable
 
 import torch
 import torch.nn.functional as F
-from overrides import override
+from overrides import overrides
 
 from allennlp.data import Vocabulary
 from allennlp.models.model import Model
@@ -171,7 +171,7 @@ class ProperRelationExtractor(Model):
         )
         return Pruner(feedforward_scorer, use_external_score=True)
 
-    @override(check_at_runtime=True)
+    @overrides
     def forward(
         self,  # type: ignore
         spans: torch.IntTensor,
@@ -359,7 +359,7 @@ class ProperRelationExtractor(Model):
         return res_dict, predictions
 
     # TODO: This code is repeated elsewhere. Refactor.
-    @override(check_at_runtime=True)
+    @overrides
     def get_metrics(self, reset: bool = False) -> Dict[str, float]:
         "Loop over the metrics for all namespaces, and return as dict."
         res = {}
