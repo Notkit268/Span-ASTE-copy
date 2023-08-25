831be2511edaf8a9ea3c0bcc1a751f574971a7d2
ugryumnik <67222376+Notkit268@users.noreply.github.com>
2023-08-25 13:50:16 +0300

    Update analysis.py

--- aste/analysis.py
+++ aste/analysis.py
@@ -23,6 +23,8 @@ def set_seed(seed: int):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
 
 
 def test_load(
